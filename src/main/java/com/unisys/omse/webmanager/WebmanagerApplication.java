1f4125479c842d10ffb107ab41b83efe6c579e6c
admin <aa@sina.com>
2018-03-07 17:08:15 +0800

    create

---
+++ src/main/java/com/unisys/omse/webmanager/WebmanagerApplication.java
@@ -0,0 +1,12 @@
+package com.unisys.omse.webmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmanagerApplication.class, args);
+	}
+}
