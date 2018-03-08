d9257411821249bb8e9a5fb85b148895da7f8053
admin <aa@sina.com>
2018-03-08 10:07:06 +0800

    update database ip from 44 to 19

--- src/main/java/com/unisys/omse/webmanager/WebmanagerApplication.java
+++ src/main/java/com/unisys/omse/webmanager/WebmanagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebmanagerApplication {
-  
+  	//Guocj
 	public static void main(String[] args) {
 		SpringApplication.run(WebmanagerApplication.class, args);
 	}
